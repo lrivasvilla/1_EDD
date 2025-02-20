8a51a4d9ec3c0e82323ae5f2bb020cb518383218
Luis R. <lrivasvilla@safareyes.es>
2025-02-20 13:30:56 +0100

    Añadiendo fechaActual

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,11 @@
+import java.time.LocalDate;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+    }
+
+    public static void fechaActual(String[] args) {
+        System.out.println(LocalDate.now());
     }
 }
